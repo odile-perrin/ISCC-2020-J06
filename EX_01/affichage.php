f4406f41c034400f465f7163b52119ddefa49927
odpr <67262187+odpr@users.noreply.github.com>
2020-06-30 11:54:09 +0200

    Push 2

--- EX_01/affichage.php
+++ EX_01/affichage.php
@@ -1,51 +1,15 @@
-
-<title>Page1</title>
-
-<?php
-function afficher_titre($nom)
-{
-    echo "<h1>Fiche produit : $nom</h1>";
-} afficher_titre('T-shirt femme');
-
-function afficher_description($couleur, $prix)
-{
-    echo "<p>Couleur : $couleur , Prix : $prix </p>";
-} afficher_description ('Rouge', '15.50');
-
-function afficher_produit($nom, $couleur, $prix, $disponible)
-{
-    echo "<p> Nom : $nom, Couleur : $couleur, Prix : $prix";
-
-}
-?>
+<?php include ("test-fonctions.php"); ?>
 
 <?php
 $nom="T-shirt femme";
 $couleur="rouge";
-$prix="15.50";
-$disponible="true";
-$disponible="false";
-$quantité="10";
-$rien="0";
+$prix=15.50;
+$disponible=true;
+$disponible=false;
+$quantité=10;
 
-echo "<h1>Fiche produit : $nom</h1>";
-echo "...";
-echo "...";
+afficher_titre($nom);
+afficher_description ($couleur, $prix);
+afficher_produit($nom, $couleur, $prix, $disponible);
 ?>
 
-Valeur de retour : Rien
-
-
-<?php
-function afficher_description($couleur, $prix)
-{
-afficher_description ;
-}
-?>
-
-
-
-paramètres: couleur, prix
-Valeur de retour : Rien
-Action: Affiche “Couleur: X, Prix: Y” dans une balise <p>. X étant la couleur passée en paramètre, Y
-étant le prix passé en paramètre.
--- EX_01/test-fonctions.php
+++ EX_01/test-fonctions.php
@@ -1,11 +1,38 @@
-<?php include ("affichage.php"); ?>
+<title>Page1</title>
+<meta charset="utf-8">
+
+<?php
+function afficher_titre($nom)
+{
+    echo "<h1>Fiche produit : $nom</h1>";
+    return;
+} 
+
+function afficher_description($couleur, $prix)
+{
+    echo "<p>Couleur : $couleur, Prix : $prix </p>";
+    return;
+} 
+
+function afficher_produit($nom, $couleur, $prix, $disponible)
+{
+    if ($disponible==true)
+    afficher_titre ($nom) and afficher_description ($couleur, $prix);
+
+    else
+    echo "<p>Le produit $nom n'est malheureusement plus disponible </p>";
+    return;
+} 
+?>
 
 <?php
 $nom="T-shirt femme";
 $couleur="rouge";
-$prix="15.50";
-$disponible="true";
-$disponible="false";
-$quantité="10";
-$rien="0";
+$prix=15.50;
+$disponible=true;
+$disponible=false;
+$quantité=10;
 ?>
+
+
+
